872785a98435ae0414f864b34fb72e43cb4488e4
zhonghanwen <zhonghanwen@conew.com>
2017-02-07 11:48:59 +0800

    修改主页内容

--- app/src/main/java/cmcm/cherry/pick/MainActivity.java
+++ app/src/main/java/cmcm/cherry/pick/MainActivity.java
@@ -1,8 +1,12 @@
 package cmcm.cherry.pick;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,8 +16,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import java.lang.reflect.InvocationTargetException;
+
+import cmcm.cherry.pick.utils.WeakHandler;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private WebView mWebView;
+    private WebViewDownloadListener mDownloadListener;
+    private ProgressBar mLoadingProgress;
+    private WeakHandler mWeakHandler = new WeakHandler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Snackbar.make(view, "MyTag:  Android Java ReactNative", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
 
@@ -38,8 +57,72 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        mLoadingProgress = (ProgressBar) findViewById(R.id.loading_progress_bar);
+        mWebView = (WebView) findViewById(R.id.wv_tutorial_webview);
+        mDownloadListener = new WebViewDownloadListener(this);
+        mWebView.setDownloadListener(mDownloadListener);
+        WebSettings webSettings = mWebView.getSettings(); // webView: 类WebView的实例
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);           //将图片调整到适合webview的大小
+        webSettings.setLoadsImagesAutomatically(true);  //支持自动加载图片
 
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+            }
+
+            //避免api23中onReceivedError方法deprecation
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                // 不要使用super，否则有些手机访问不了，因为包含了一条 handler.cancel()
+//                super.onReceivedSslError(view, handler, error);
+                handleSslError(handler);
+            }
+
+
+        });
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+
+            }
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (mLoadingProgress != null) {
+                    mLoadingProgress.setVisibility(View.VISIBLE);
+                    mLoadingProgress.setProgress(newProgress);
+                    if (newProgress == 100) {
+                        mLoadingProgress.setVisibility(View.GONE);
+                    }
+                }
+            }
+        });
+
+        mLoadingProgress.setVisibility(View.VISIBLE);
+
+        loadUrl("https://github.com/zhonghanwen");
     }
+
 
     @Override
     public void onBackPressed() {
@@ -81,8 +164,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            AwesomeWebViewActivity.launch(this, null, "https://github.com/zhonghanwen");
         } else if (id == R.id.nav_gallery) {
-
+            AwesomeWebViewActivity.launch(this, null, "http://www.cnblogs.com/common1140/");
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -96,5 +180,123 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+
+
+    private void handleSslError(final SslErrorHandler handler) {
+
+    }
+
+
+    private void doExit(boolean finish) {
+        if (!finish && mWebView != null && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return;
+        }
+        finish();
+
+    }
+
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+
+
+    private void loadUrl(final String url) {
+        mWeakHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (TextUtils.isEmpty(url)) {
+                    return;
+                }
+                try {
+                    mWebView.loadUrl(url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mWebView != null) {
+            mWebView.destroy();
+            mWebView = null;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            if (mWebView != null) {
+                Class.forName("android.webkit.WebView").getMethod("onPause", (Class[]) null).invoke(mWebView, (Object[]) null);
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            if (mWebView != null) {
+                Class.forName("android.webkit.WebView").getMethod("onResume", (Class[]) null).invoke(mWebView, (Object[]) null);
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 可能可以避免内存泄露
+        mWebView.destroy();
+        mWebView = null;
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            doExit(false);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
